3d98e575d43451b3bc3241dfa1c1fbd73583513b
Onkar Yadav <onkarumashankaryadav@gmail.com>
2026-01-17 23:01:48 +0100

    chore: initialize project structure with Spring Boot and Java 21

---
+++ src/main/java/com/student/eurosafe/EuroSafeApplication.java
@@ -0,0 +1,13 @@
+package com.student.eurosafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuroSafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuroSafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/student/eurosafe/EuroSafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.eurosafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuroSafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
