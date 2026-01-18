c3b7323bee3cf5f713c66e6517e9075c3b68818c
Onkar Yadav <onkarumashankaryadav@gmail.com>
2026-01-18 20:33:50 +0100

    feat: create User entity with JPA annotations

---
+++ src/main/java/com/student/eurosafe/entity/User.java
@@ -0,0 +1,27 @@
+package com.student.eurosafe.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "users") // "user" is a reserved word in Postgres, so we use "users"
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash; // We never store plain passwords!
+}
